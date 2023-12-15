4e185c5f20e9560c6ac7f5f9cfa817c8127d198a
jomasator2 <josator2infupv@gmail.com>
2023-12-15 13:57:27 +0100

    Refactor main function and add command line arguments

--- src/dcm2mids/__main__.py
+++ src/dcm2mids/__main__.py
@@ -1,20 +1,39 @@
 import argparse
+
 # Importante solo utilizar el modulo Path de pathlib.
 from pathlib import Path
 
-# Path: __main__.py
+
+def main():
+    """
+    Main entry point allowing external calls
+    """
+
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description="Script for reading a folder with images and converting them to a BIDS/MIDS Structure.",
 )
 
-parser.add_argument("-i", "--input", type=Path, help="Path to the input folder", required=True)
-parser.add_argument("-o", "--output", type=Path, help="Path to the output folder", required=True)
-#parser.add_argument('-bp', '--body-part', dest="body_part", type=str,
-#                   help="""Specify which part of the body are 
-#                   in the dataset(REQUIRED)""", required=True)
+parser.add_argument(
+    "-i", "--input", type=Path, help="Path to the input folder", required=True
+)
+parser.add_argument(
+    "-o", "--output", type=Path, help="Path to the output folder", required=True
+)
+parser.add_argument(
+    "-bp",
+    "--body-part",
+    dest="body_part",
+    type=str,
+    help="""Specify which part of the body are in the dataset(REQUIRED)""",
+    required=True,
+)
+parser.add_argument("-b", "--bids", dest="bids", action="store_true", help="use BIDS standard. Only applicable for protocols/body parts considered in BIDS.")
 args = parser.parse_args()
 
+get_dicomdir(args.input)
 
+create_mids_directory(args.input, args.output, args.body_part)
 
+generate_tsvs(args.output)
