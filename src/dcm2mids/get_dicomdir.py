4d915f6b324f3127259b9ad7cf635a48f7580ce6
jomasator2 <josator2infupv@gmail.com>
2023-12-20 11:39:52 +0100

    Add get_dicomdir function to retrieve DICOMDIR file
    This commit adds a new function, get_dicomdir, to the get_dicomdir.py file. This function improves the functionality of the code by providing a way to access DICOM information.

---
+++ src/dcm2mids/get_dicomdir.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from typing import Union
+
+from pydicom import dcmread
+from pydicom.fileset import FileSet
+
+
+def get_dicomdir(input_folder: Union[Path, str]) -> FileSet:
+    """
+    Get the DICOMDIR file from the input folder.
+    """
+    if not isinstance(input_folder, Path):
+        try:
+            input_folder = Path(input_folder)
+        except TypeError:
+            raise TypeError(
+                f"Input folder must be a Path object or a string. Got {type(input_folder)} instead."
+            )
+    if not input_folder.exists():
+        raise FileNotFoundError(f"Input folder {input_folder} does not exist.")
+    dicomdir = input_folder / "DICOMDIR"
+    if dicomdir.exists():  # If DICOMDIR exists, we will use it to get the DICOM str.
+        ds = dcmread(dicomdir)
+        fs = FileSet(ds)
+    else:  # If DICOMDIR does not exist, we will search for DICOM files in the input folder.
+        fs = FileSet()
+        for file in dicomdir.glob("*.dcm"):
+            fs.add(path)
+    return fs
