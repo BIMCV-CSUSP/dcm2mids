7f248d11d89e0c99f4c226e6a8700836e3292b1a
jomasator2 <josator2infupv@gmail.com>
2023-12-20 11:40:57 +0100

    Refactor command line argument parsing in __main__.py

--- src/dcm2mids/__main__.py
+++ src/dcm2mids/__main__.py
@@ -1,5 +1,4 @@
 import argparse
-
 # Importante solo utilizar el modulo Path de pathlib.
 from pathlib import Path
 
@@ -29,7 +28,13 @@ parser.add_argument(
     help="""Specify which part of the body are in the dataset(REQUIRED)""",
     required=True,
 )
-parser.add_argument("-b", "--bids", dest="bids", action="store_true", help="use BIDS standard. Only applicable for protocols/body parts considered in BIDS.")
+parser.add_argument(
+    "-b",
+    "--bids",
+    dest="bids",
+    action="store_true",
+    help="use BIDS standard. Only applicable for protocols/body parts considered in BIDS.",
+)
 args = parser.parse_args()
 
 get_dicomdir(args.input)
