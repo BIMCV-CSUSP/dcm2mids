6ffd7dd763c175d6b45dbc4d47408a99670bd3e4
jomasator2 <josator2infupv@gmail.com>
2024-01-08 15:12:43 +0100

    Adding magnetic resonance procedure class

---
+++ src/dcm2mids/procedures/magnetic_resonance/magnetic_resonance_procedure.py
@@ -0,0 +1,20 @@
+from dcm2mids.procedures import Procedures
+
+# TODO: convert to nifti
+# TODO: 
+class ProceduresMegneticResonance(Procedures):
+    def __init__(self):
+        pass
+
+    def get_mids_path(self):
+        pass
+
+    def determine_image_type(self):
+        pass
+
+    def control_session_image(self):
+        #TODO: 
+        pass
+
+    def get_name(self):
+        pass
