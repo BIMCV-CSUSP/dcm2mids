f47a7e0e2d3ce4a8b799b658fca1445f3ef26062
jomasator2 <josator2infupv@gmail.com>
2024-01-08 15:13:50 +0100

    Add dicom2nifti function to convert DICOM files to NIfTI format

---
+++ src/dcm2mids/procedures/dicom2nifti.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from typing import Union
+
+def dicom2nifti(json_files: Union[str, Path ,list], path_to_save:Union[str, Path]) -> Path:
+    folder_nifti = folder_json.parent.parent.joinpath("LOCAL_NIFTI", "files")
+    folder_nifti.mkdir(parents=True, exist_ok=True)
+    d2n.convert_directory(str(folder_json), str(folder_nifti))
+    shutil.copy2(str(folder_json.joinpath("bids.json")), str(folder_nifti.parent))
+    if folder_json.joinpath("note.txt").exists():
+        shutil.copy2(str(folder_json.joinpath("note.txt")), str(folder_nifti.parent))
+    return folder_nifti.parent
