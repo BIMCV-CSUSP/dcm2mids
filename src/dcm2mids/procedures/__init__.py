2494688d501c62f7bbadef55843cb9ba4df5dc67
jomasator2 <josator2infupv@gmail.com>
2024-02-28 16:55:49 +0100

    Add initialization variables in Procedures class

--- src/dcm2mids/procedures/__init__.py
+++ src/dcm2mids/procedures/__init__.py
@@ -1,10 +1,13 @@
-import dicom2nifti as d2n
+
 from abc import ABC, abstractmethod
 
 
 class Procedures(ABC):
-    def __int__(self):
-        pass
+    def __init__(self, mids_path, bodypart):
+        
+        self.mids_path = mids_path
+        self.bodypart = bodypart
+        
 
     @abstractmethod
     def control_session_image(self):
