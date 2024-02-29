e967f857787185d0d759d6114e95c7b60b579fed
jomasator2 <josator2infupv@gmail.com>
2024-02-29 16:19:35 +0100

    Add use_bodypart and use_viewposition parameters to Procedures constructor

--- src/dcm2mids/procedures/__init__.py
+++ src/dcm2mids/procedures/__init__.py
@@ -5,10 +5,13 @@ from dcm2mids.procedures.dictify import dictify
 import json
 
 class Procedures(ABC):
-    def __init__(self, mids_path, bodypart):
+    def __init__(self, mids_path, bodypart, use_bodypart, use_viewposition):
         
         self.mids_path = mids_path
         self.bodypart = bodypart
+        self.use_bodypart = use_bodypart
+        self.use_viewposition = use_viewposition
+        self.chunk : bool
         
 
     # @abstractmethod
