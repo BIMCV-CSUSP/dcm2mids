fadcb2435fafad54ea5856cb8918b5cc0c683e24
jomasator2 <josator2infupv@gmail.com>
2024-02-29 10:44:26 +0100

    Add convert_to_jsonfile method to Procedures class

--- src/dcm2mids/procedures/__init__.py
+++ src/dcm2mids/procedures/__init__.py
@@ -1,6 +1,8 @@
 
 from abc import ABC, abstractmethod
 
+from dcm2mids.procedures.dictify import dictify
+import json
 
 class Procedures(ABC):
     def __init__(self, mids_path, bodypart):
@@ -9,10 +11,20 @@ class Procedures(ABC):
         self.bodypart = bodypart
         
 
-    @abstractmethod
-    def control_session_image(self):
-        pass
+    # @abstractmethod
+    # def control_session_image(self):
+    #     pass
 
     @abstractmethod
     def get_name(self):
+        pass
+
+    def convert_to_jsonfile(self, dataset, file_path_mids):
+        json_dict = dictify(dataset)
+        file_path_mids.parent.mkdir(parents=True, exist_ok=True)
+        with open(file_path_mids, "w") as f:
+            json.dump(json_dict, f, indent=4)
+
+    @abstractmethod
+    def run(self):
         pass
