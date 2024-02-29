2a477fb8fb009c828bafc5e346c2fdb854007c2a
jomasator2 <josator2infupv@gmail.com>
2024-02-29 10:45:48 +0100

    Add logic to create MIDS directory structure and run procedures for MR and VL modalities

--- src/dcm2mids/create_mids_directory.py
+++ src/dcm2mids/create_mids_directory.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 from typing import Union
 
+from .procedures.magnetic_resonance.magnetic_resonance_procedure import (
+    ProceduresMagneticResonance,
+)
+from .procedures.visible_light.visible_light_procedure import ProceduresVisibleLight
 from pydicom.fileset import FileSet
-from pydicom.dataset import Dataset
+
+
 # TODO: Add the logic to create the MIDS directory structure.
 def create_mids_directory(
     fileset: FileSet, mids_path: Union[Path, str], bodypart: str
 ) -> None:
     """Create the MIDS directory structure."""
-    
-    
+    use_bodypart = len(fileset.find_values("BodyPartExamined")) > 1  ### []
+    use_viewposition = len(fileset.find_values("ViewPosition")) > 1  ### []
+    procedure_MR = ProceduresMagneticResonance(mids_path, bodypart)
+    procedure_VL = ProceduresVisibleLight(mids_path, bodypart)
+
     for subject in fileset.find_values("PatientID"):
         print(f"{subject=}")
         for session in fileset.find_values("StudyID", fileset.find(PatientID=subject)):
@@ -22,24 +30,22 @@ def create_mids_directory(
                 for instance in fileset.find(
                     PatientID=subject, StudyID=session, SeriesNumber=scan
                 ):
-                    instance_list.append(( instance))
-                if instance_list[0].Modality == "MR":
-                    print(instance_list[0].path)
-                    pass
-                if instance_list[0].Modality in ["CR", "DX", "CT", "PT"]:
-                    print(instance_list[0].path)
-                    pass
-                if instance_list[0].Modality in ["OP", "SC", "XC", "OT", "SM"]:
-                    print(instance_list[0].path)
-                    pass
-# TODO: Add the logic to create the TSV files.
+                    try:
+                        instance_number = int(instance.InstanceNumber)
+                    except (AttributeError, ValueError):
+                        instance_number = -1
+                    instance_list.append((instance_number, instance))
 
-def dictify(ds: Dataset) -> dict:
-    """Turn a pydicom Dataset into a dict with keys derived from the Element names."""
-    output = dict()
-    for elem in ds:
-        if elem.VR != "SQ":
-            output[elem.name] = str(elem.value)
-        else:
-            output[elem.name] = [dictify(item) for item in elem]
-    return output
+                if instance_list[0][1].Modality == "MR":
+                    procedure_MR.run(instance_list)
+                    pass
+                if instance_list[0][1].Modality in ["CR", "DX", "CT", "PT"]:
+
+                    pass
+                if instance_list[0][1].Modality in ["OP", "SC", "XC", "OT", "SM", "BF"]:
+                    procedure_VL.run(instance_list)
+
+                    pass
+
+
+# TODO: Add the logic to create the TSV files.
