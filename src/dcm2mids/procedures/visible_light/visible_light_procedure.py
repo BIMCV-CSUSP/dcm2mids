5c25c6a42f7d749de1ff38e8d6a2f0b6ba6c67bc
Alejandro <morarubioa@gmail.com>
2024-03-04 12:01:03 +0000

    Add docstrings and typehints for visible light procedure

--- src/dcm2mids/procedures/visible_light/visible_light_procedure.py
+++ src/dcm2mids/procedures/visible_light/visible_light_procedure.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+from typing import List, Tuple
 
 import SimpleITK as sitk
+from pydicom import Dataset
+from pydicom.fileset import FileInstance
 
 from .. import Procedures
 
@@ -11,6 +14,8 @@ from .. import Procedures
 # TODO: rename nifti file to BIDS/MIDS standard
 #
 class ProceduresVisibleLight(Procedures):
+    """Conversion logic for Visible Light Imaging procedures."""
+
     def __init__(
         self,
         mids_path: Path,
@@ -24,7 +29,17 @@ class ProceduresVisibleLight(Procedures):
     def reset(self):
         self.dicom_dict = {}
 
-    def classify_image_type(self, instance):
+    def classify_image_type(
+        self, instance: FileInstance
+    ) -> Tuple[str, Tuple[str, ...]]:
+        """
+        Classifies an image based on its modality.
+
+        :param instance: The instance to be classified.
+        :type instance: pydicom.fileset.FileInstance
+        :returns: A tuple containing the image type and a tuple of labels for that type.
+        :rtype: tuple[str, tuple[str, ...]]
+        """
         print(instance)
         if instance.Modality in ["OP", "SC", "XC", "OT"]:
             return ("op", ("mim-light", "op"))
@@ -32,7 +47,20 @@ class ProceduresVisibleLight(Procedures):
             return ("BF", ("micr",))
         return ("", tuple())
 
-    def get_name(self, dataset, modality, mim):
+    def get_name(self, dataset: Dataset, modality: str, mim: Tuple[str, ...]) -> Path:
+        """
+        Generates a name for the image based on its metadata.
+
+        :param dataset: The dataset containing the image metadata.
+        :type dataset: pydicom.Dataset
+        :param modality: The modality of the image.
+        :type modality: str
+        :param mim: A tuple of labels to be included in the filepath.
+        :type mim: tuple[str, ...]
+        :returns: A Path object representing the generated name.
+        :rtype: pathlib.Path
+        """
+
         sub = f"sub-{dataset.PatientID}"
         ses = f"ses-{dataset.StudyID}"
         run = (
@@ -69,12 +97,28 @@ class ProceduresVisibleLight(Procedures):
             ),
         )
 
-    def convert_to_image(self, instance, file_path_mids):
+    def convert_to_image(self, instance: FileInstance, file_path_mids: Path):
+        """
+        Converts a DICOM to an image.
+
+        :param instance: The DICOM image instance.
+        :type instance: pydicom.fileset.FileInstance
+        :param file_path_mids: The path where the converted image will be saved.
+        :type file_path_mids: pathlib.Path
+        """
+
         file_path_mids.parent.mkdir(parents=True, exist_ok=True)
         image = sitk.ReadImage(instance.path)
         sitk.WriteImage(image, file_path_mids)
 
-    def run(self, instance_list: list):
+    def run(self, instance_list: List[Tuple[int, FileInstance]]):
+        """
+        Runs the image conversion pipeline on a list of instances.
+
+        :param instance_list: A list of tuples containing the instance number and the DICOM instance.
+        :type instance_list: list[uuple[int, pydicom.fileset.FileInstance]]
+        """
+
         self.use_chunk = len(instance_list) > 1
         for _, instance in sorted(instance_list, key=lambda x: x[0]):
             print(instance)
