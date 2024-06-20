547dff046a091d9366292a0e6ec7a125d80176a7
jomasator2 <josator2infupv@gmail.com>
2024-06-20 15:08:32 +0200

    chore: Refactor dictify function to handle private tags and convert element names to camel case

--- src/dcm2mids/procedures/dictify.py
+++ src/dcm2mids/procedures/dictify.py
@@ -1,5 +1,11 @@
 from pydicom.dataset import Dataset
+from pydicom.datadict import get_entry
 
+
+def convert_string(input_string: str) -> str:
+    """Split the string into words, capitalize each word, and then join them without spaces"""
+    cleaned_string = input_string.replace('(', ' ').replace(')', ' ')
+    return ''.join(word.capitalize() for word in cleaned_string.split())
 
 def dictify(ds: Dataset, stop_before_pixels: bool = True) -> dict:
     """Turn a pydicom Dataset into a dict with keys derived from the Element names."""
@@ -8,7 +14,13 @@ def dictify(ds: Dataset, stop_before_pixels: bool = True) -> dict:
         if elem.name == "Pixel Data" and stop_before_pixels:
             continue
         if elem.VR != "SQ":
-            output[elem.name] = str(elem.value)
+            if not elem.tag.is_private:
+                output[get_entry(elem.tag)[-1]] = str(elem.value)
+            else:
+                output[convert_string(elem.name)] = str(elem.value)
         else:
-            output[elem.name] = [dictify(item) for item in elem]
+            if not elem.tag.is_private:
+                output[get_entry(elem.tag)[-1]] = [dictify(item) for item in elem]
+            else:
+                output[convert_string(elem.name)] = [dictify(item) for item in elem]  
     return output
