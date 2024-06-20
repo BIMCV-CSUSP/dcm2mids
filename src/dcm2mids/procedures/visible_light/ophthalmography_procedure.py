8c2913c5e75a7cf2d333dc602f3d7f83949784c3
jomasator2 <josator2infupv@gmail.com>
2024-06-20 15:09:14 +0200

    Refactor OphthalmographyProcedures class to include NumberOfFrames attribute

--- src/dcm2mids/procedures/visible_light/ophthalmography_procedure.py
+++ src/dcm2mids/procedures/visible_light/ophthalmography_procedure.py
@@ -67,6 +67,7 @@ class OphthalmographyProcedures(Procedures):
                 "PhotometricInterpretation",
                 "ImagedVolumeHeight",
                 "ImagedVolumeWeight",
+                "NumberOfFrames"
             ]
             return ("BF", ("micr",))
         return ("", tuple())
